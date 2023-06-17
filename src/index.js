0884af22c21166496451557cce9104b2f7146a26
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-17 18:57:15 +0100

    Fixed bug...

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import ReactDOM from 'react-dom/client';
 import './styles/output.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
