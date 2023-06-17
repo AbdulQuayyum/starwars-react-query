f9cc5f191c4a2851f0e1bc05f9b684ce21881f49
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-06-17 19:05:21 +0100

    Imported css file...

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import ReactDOM from 'react-dom/client';
 import './styles/output.css';
+import './styles/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
